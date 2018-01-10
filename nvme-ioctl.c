a87393928cbf7890249197396ad5ad2ba060659d
Minwoo Im <minwoo.im.dev@gmail.com>
2018-01-10 20:04:45 +0900

    nvme-cli: Remove unnecessary nsid field in error-log
    Git-commit: 307929b2641ee974e71f6b14d80da5ebed21e507
    References: bsc#1076004
    
    Error information log page is global to controller.
    
    Signed-off-by: Minwoo Im <minwoo.im.dev@gmail.com>

--- nvme-ioctl.c
+++ nvme-ioctl.c
@@ -405,8 +405,7 @@ int nvme_fw_log(int fd, struct nvme_firmware_log_page *fw_log)
 	return nvme_get_log(fd, NVME_NSID_ALL, NVME_LOG_FW_SLOT, sizeof(*fw_log), fw_log);
 }
 
-int nvme_error_log(int fd, __u32 nsid, int entries,
-		   struct nvme_error_log_page *err_log)
+int nvme_error_log(int fd, int entries, struct nvme_error_log_page *err_log)
 {
 	return nvme_get_log(fd, 0, NVME_LOG_ERROR, entries * sizeof(*err_log), err_log);
 }
