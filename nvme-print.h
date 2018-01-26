7405f30156a6b16153d0a5cc1b23390953068d60
Minwoo Im <minwoo.im.dev@gmail.com>
2018-01-27 00:36:19 +0900

    nvme-cli: move print-function to nvme-print
    Git-commit: e201d431ac715754f929fd9faa8988246e67e17c
    References: bsc#1099018
    
    Code clean-up by moving print-functions for list and list-subsys
    commands to nvme-print.c with no functional changes.
    
    Signed-off-by: Minwoo Im <minwoo.im.dev@gmail.com>

--- nvme-print.h
+++ nvme-print.h
@@ -29,6 +29,8 @@ void show_fw_log(struct nvme_firmware_log_page *fw_log, const char *devname);
 void show_effects_log(struct nvme_effects_log_page *effects);
 void show_ctrl_registers(void *bar, unsigned int mode, bool fabrics);
 void show_nvme_id_ns_descs(void *data);
+void show_list_items(struct list_item *list_items, unsigned len);
+void show_nvme_subsystem_list(struct subsys_list_item *slist, int n);
 
 void nvme_feature_show_fields(__u32 fid, unsigned int result, unsigned char *buf);
 void nvme_directive_show_fields(__u8 dtype, __u8 doper, unsigned int result, unsigned char *buf);
